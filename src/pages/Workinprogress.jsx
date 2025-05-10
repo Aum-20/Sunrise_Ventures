5da81735be0e91e7139468c278237d46912dc059
Sanket Nabade <sanketnabade2004@gmail.com>
2025-05-10 23:22:27 +0530

    Initial frontend setup

---
+++ src/pages/Workinprogress.jsx
@@ -0,0 +1,20 @@
+import "../assets/css/workInProgress.css";
+
+export default function Workinprogress() {
+  return (
+    <div>
+
+    
+    <title>Under Construction</title>
+    
+
+
+    <div className="container">
+        <img src="https://img.icons8.com/ios-filled/100/000000/maintenance.png" alt="Under Construction" />
+        <h1>We're Working On This!</h1>
+        <p>Our team is currently working on this page. Please check back soon for updates.</p>
+        <a href="/home">Go Back to Home</a>
+    </div>
+    </div>
+  );
+}
